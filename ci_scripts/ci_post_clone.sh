d6e46a222d5f0be9fc0bca5eda576d2274906119
Surya <suryaramadhani98@gmail.com>
2026-09-03 15:53:14 +0700

    Install CocoaPods dependencies in Xcode Cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,3 +8,11 @@ defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -b
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
 echo "=== Package plugin validation disabled ==="
+
+echo "=== Installing CocoaPods dependencies ==="
+
+cd "$CI_PRIMARY_REPOSITORY_PATH"
+
+pod install --repo-update
+
+echo "=== CocoaPods installation completed ==="
